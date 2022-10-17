f5c071571a1f387ec6bb2a602851d44cece7ab17
Hanarinder Singh <harnarinder.singh@yum.com>
2022-10-17 22:43:21 +0530

    feat: added transfer function and event

--- src/contracts/HarryToken.sol
+++ src/contracts/HarryToken.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.7;
 
 contract HarryToken {
+    string public name = 'HarryToken';
+    string public symbol = 'HT';
+    string public standard = 'HarryToken v1.0';
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-    constructor() {
-        totalSupply = 1000000;
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] < _value && _value > 0);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
     }
 }
--- src/test/HarryToken.test.js
+++ src/test/HarryToken.test.js
@@ -4,6 +4,7 @@ import assert from 'assert';
 import { beforeEach, describe, it } from 'mocha';
 import output from '../compile';
 
+const INITIAL_SUPPLY = 1000000;
 const web3 = new Web3(ganache.provider());
 
 let accounts;
@@ -16,17 +17,47 @@ beforeEach(async () => {
     contract = await new web3.eth.Contract(output.abi)
         .deploy({
             data: output.evm.bytecode.object,
+            arguments: [INITIAL_SUPPLY],
         })
         .send({ from: managerAccount, gas: '1000000' });
 });
 
 describe('Harry Token', () => {
-    it('Contract gets deployed', async () => {
+    it('Contract gets deployed with correct values', async () => {
         assert.ok(contract.options.address);
+        const name = await contract.methods.name().call();
+        const symbol = await contract.methods.symbol().call();
+        assert(name, 'Harry Token');
+        assert(symbol, 'HT');
     });
 
     it('Total supply check', async () => {
         const totalSupply = await contract.methods.totalSupply().call();
-        assert(totalSupply, 1000000);
+        assert(totalSupply, INITIAL_SUPPLY);
+    });
+
+    it('Check balance for owner account', async () => {
+        const balanceOfManager = await contract.methods
+            .balanceOf(managerAccount)
+            .call();
+        assert(balanceOfManager, INITIAL_SUPPLY);
+    });
+
+    it('checks transfer function and transfer event', async () => {
+        const [, secondAccount] = accounts;
+        const transferAmount = 100000;
+        const transfer = await contract.methods.transfer(
+            secondAccount,
+            transferAmount
+        );
+        const firstAccountBal = await contract.methods
+            .balanceOf(managerAccount)
+            .call();
+        const secondAccountBal = await contract.methods
+            .balanceOf(secondAccount)
+            .call();
+        assert(firstAccountBal, INITIAL_SUPPLY - transferAmount);
+        assert(secondAccountBal, transferAmount);
+        console.log(transfer);
     });
 });
